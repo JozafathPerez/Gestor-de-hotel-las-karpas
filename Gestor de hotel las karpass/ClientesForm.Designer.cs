2aed6f4afdb966ab570ca1dcedcf16d4cb2153eb
Jozafath Perez <jozperez@estudiantec.cr>
2024-04-10 09:33:35 -0600

    Clase FuncionesAux y base de ClientesForm

--- Gestor de hotel las karpass/ClientesForm.Designer.cs
+++ Gestor de hotel las karpass/ClientesForm.Designer.cs
@@ -28,28 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.DataViewClientes = new System.Windows.Forms.DataGridView();
+            this.BtActualizar = new System.Windows.Forms.Button();
+            this.BtEliminar = new System.Windows.Forms.Button();
+            this.BtGuardar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textTelefono = new System.Windows.Forms.TextBox();
+            this.textCorreo = new System.Windows.Forms.TextBox();
+            this.textFechaNacimiento = new System.Windows.Forms.TextBox();
+            this.textDireccion = new System.Windows.Forms.TextBox();
+            this.textPais = new System.Windows.Forms.TextBox();
+            this.textApellido2 = new System.Windows.Forms.TextBox();
+            this.textApellido1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataViewClientes)).BeginInit();
             this.SuspendLayout();
+            // 
+            // DataViewClientes
+            // 
+            this.DataViewClientes.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DataViewClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataViewClientes.Location = new System.Drawing.Point(492, 39);
+            this.DataViewClientes.Name = "DataViewClientes";
+            this.DataViewClientes.Size = new System.Drawing.Size(472, 477);
+            this.DataViewClientes.TabIndex = 0;
+            // 
+            // BtActualizar
+            // 
+            this.BtActualizar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.BtActualizar.Location = new System.Drawing.Point(526, 536);
+            this.BtActualizar.Name = "BtActualizar";
+            this.BtActualizar.Size = new System.Drawing.Size(160, 40);
+            this.BtActualizar.TabIndex = 1;
+            this.BtActualizar.Text = "ACTUALIZAR";
+            this.BtActualizar.UseVisualStyleBackColor = true;
+            this.BtActualizar.Click += new System.EventHandler(this.BtActualizar_Click);
+            // 
+            // BtEliminar
+            // 
+            this.BtEliminar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.BtEliminar.Location = new System.Drawing.Point(777, 536);
+            this.BtEliminar.Name = "BtEliminar";
+            this.BtEliminar.Size = new System.Drawing.Size(160, 40);
+            this.BtEliminar.TabIndex = 2;
+            this.BtEliminar.Text = "ELIMINAR";
+            this.BtEliminar.UseVisualStyleBackColor = true;
+            this.BtEliminar.Click += new System.EventHandler(this.BtEliminar_Click);
+            // 
+            // BtGuardar
+            // 
+            this.BtGuardar.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.BtGuardar.Location = new System.Drawing.Point(155, 476);
+            this.BtGuardar.Name = "BtGuardar";
+            this.BtGuardar.Size = new System.Drawing.Size(160, 40);
+            this.BtGuardar.TabIndex = 3;
+            this.BtGuardar.Text = "GUARDAR";
+            this.BtGuardar.UseVisualStyleBackColor = true;
+            this.BtGuardar.Click += new System.EventHandler(this.BtGuardar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("OCR A Extended", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(61, 294);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(60, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(875, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "\"Panel\" (Forms en realidad) de Clientes";
+            this.label1.Size = new System.Drawing.Size(68, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.Location = new System.Drawing.Point(60, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Primer apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label3.Location = new System.Drawing.Point(60, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Segundo apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label4.Location = new System.Drawing.Point(60, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "País Origen";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label5.Location = new System.Drawing.Point(60, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Dirección";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label6.Location = new System.Drawing.Point(60, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 21);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Fecha Nacimiento";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label7.Location = new System.Drawing.Point(60, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 21);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Telefono";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label8.Location = new System.Drawing.Point(60, 366);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 21);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Correo";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(230, 36);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(208, 23);
+            this.textNombre.TabIndex = 12;
+            // 
+            // textTelefono
+            // 
+            this.textTelefono.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTelefono.Location = new System.Drawing.Point(230, 319);
+            this.textTelefono.Name = "textTelefono";
+            this.textTelefono.Size = new System.Drawing.Size(208, 23);
+            this.textTelefono.TabIndex = 13;
+            // 
+            // textCorreo
+            // 
+            this.textCorreo.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCorreo.Location = new System.Drawing.Point(230, 366);
+            this.textCorreo.Name = "textCorreo";
+            this.textCorreo.Size = new System.Drawing.Size(208, 23);
+            this.textCorreo.TabIndex = 14;
+            // 
+            // textFechaNacimiento
+            // 
+            this.textFechaNacimiento.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFechaNacimiento.Location = new System.Drawing.Point(230, 272);
+            this.textFechaNacimiento.Name = "textFechaNacimiento";
+            this.textFechaNacimiento.Size = new System.Drawing.Size(208, 23);
+            this.textFechaNacimiento.TabIndex = 15;
+            // 
+            // textDireccion
+            // 
+            this.textDireccion.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDireccion.Location = new System.Drawing.Point(230, 227);
+            this.textDireccion.Name = "textDireccion";
+            this.textDireccion.Size = new System.Drawing.Size(208, 23);
+            this.textDireccion.TabIndex = 16;
+            // 
+            // textPais
+            // 
+            this.textPais.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textPais.Location = new System.Drawing.Point(230, 178);
+            this.textPais.Name = "textPais";
+            this.textPais.Size = new System.Drawing.Size(208, 23);
+            this.textPais.TabIndex = 17;
+            // 
+            // textApellido2
+            // 
+            this.textApellido2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textApellido2.Location = new System.Drawing.Point(230, 135);
+            this.textApellido2.Name = "textApellido2";
+            this.textApellido2.Size = new System.Drawing.Size(208, 23);
+            this.textApellido2.TabIndex = 18;
+            // 
+            // textApellido1
+            // 
+            this.textApellido1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textApellido1.Location = new System.Drawing.Point(230, 91);
+            this.textApellido1.Name = "textApellido1";
+            this.textApellido1.Size = new System.Drawing.Size(208, 23);
+            this.textApellido1.TabIndex = 19;
             // 
             // ClientesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(996, 625);
+            this.Controls.Add(this.textApellido1);
+            this.Controls.Add(this.textApellido2);
+            this.Controls.Add(this.textPais);
+            this.Controls.Add(this.textDireccion);
+            this.Controls.Add(this.textFechaNacimiento);
+            this.Controls.Add(this.textCorreo);
+            this.Controls.Add(this.textTelefono);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtGuardar);
+            this.Controls.Add(this.BtEliminar);
+            this.Controls.Add(this.BtActualizar);
+            this.Controls.Add(this.DataViewClientes);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ClientesForm";
             this.Text = "ClientesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.DataViewClientes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +273,25 @@
 
         #endregion
 
+        private System.Windows.Forms.DataGridView DataViewClientes;
+        private System.Windows.Forms.Button BtActualizar;
+        private System.Windows.Forms.Button BtEliminar;
+        private System.Windows.Forms.Button BtGuardar;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textTelefono;
+        private System.Windows.Forms.TextBox textCorreo;
+        private System.Windows.Forms.TextBox textFechaNacimiento;
+        private System.Windows.Forms.TextBox textDireccion;
+        private System.Windows.Forms.TextBox textPais;
+        private System.Windows.Forms.TextBox textApellido2;
+        private System.Windows.Forms.TextBox textApellido1;
     }
 }
--- Gestor de hotel las karpass/ClientesForm.cs
+++ Gestor de hotel las karpass/ClientesForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,12 +16,130 @@ namespace Gestor_de_hotel_las_karpass
         private ConexionBD conexion;
         private FuncionesAux funcionesAux;
         private int idEmpleado;
+
         public ClientesForm(int idEmpleado)
         {
             InitializeComponent();
             this.idEmpleado = idEmpleado;
             conexion = new ConexionBD();
             funcionesAux = new FuncionesAux(conexion);
+            actualizarDataView();
         }
+
+        public void actualizarDataView()
+        {
+            conexion.abrir();
+            string query = "SELECT identificacionCliente AS 'IdCliente', nombre AS 'Nombre', paisProcedencia AS 'Pais Origen', telefono AS 'Telefono', correo AS 'Correo' FROM Clientes";
+            SqlCommand comando = new SqlCommand(query, conexion.ConectarBD);
+            SqlDataAdapter data = new SqlDataAdapter(comando);
+            DataTable tabla = new DataTable();
+            data.Fill(tabla);
+            DataViewClientes.DataSource = tabla;
+            conexion.cerrar();
+        }
+
+        private void BtGuardar_Click(object sender, EventArgs e)
+        {
+            // Verifica si hay datos en los campos obligatorios
+            if (string.IsNullOrWhiteSpace(textNombre.Text) || string.IsNullOrWhiteSpace(textApellido1.Text) ||
+                string.IsNullOrWhiteSpace(textPais.Text) || string.IsNullOrWhiteSpace(textDireccion.Text) ||
+                string.IsNullOrWhiteSpace(textFechaNacimiento.Text) || string.IsNullOrWhiteSpace(textTelefono.Text) ||
+                string.IsNullOrWhiteSpace(textCorreo.Text))
+            {
+                MessageBox.Show("Por favor, complete todos los campos obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string query = "INSERT INTO Clientes (nombre, primerApellido, segundoApellido, paisProcedencia, direccion, fechaNacimiento, telefono, correo)" +
+                           "VALUES (@nombre, @primerApellido, @segundoApellido, @paisProcedencia, @direccion, @fechaNacimiento, @telefono, @correo)";
+            SqlCommand comando = new SqlCommand(query, conexion.ConectarBD);
+            comando.Parameters.AddWithValue("@nombre", textNombre.Text);
+            comando.Parameters.AddWithValue("@primerApellido", textApellido1.Text);
+            comando.Parameters.AddWithValue("@segundoApellido", textApellido2.Text);
+            comando.Parameters.AddWithValue("@paisProcedencia", textPais.Text);
+            comando.Parameters.AddWithValue("@direccion", textDireccion.Text);
+            comando.Parameters.AddWithValue("@fechaNacimiento", textFechaNacimiento.Text);
+            comando.Parameters.AddWithValue("@telefono", textTelefono.Text);
+            comando.Parameters.AddWithValue("@correo", textCorreo.Text);
+
+            try
+            {
+                conexion.abrir();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("La persona: " + textNombre.Text + " se ha agregado correctamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                actualizarDataView();
+
+                textNombre.Text = "";
+                textApellido1.Text = "";
+                textApellido2.Text = "";
+                textPais.Text = "";
+                textDireccion.Text = "";
+                textFechaNacimiento.Text = "";
+                textTelefono.Text = "";
+                textCorreo.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar la persona: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexion.cerrar();
+            }
+        }
+
+
+        private void BtActualizar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtEliminar_Click(object sender, EventArgs e)
+        {
+            // Verifica si hay una fila seleccionada en el DataGridView
+            if (DataViewClientes.SelectedRows.Count > 0)
+            {
+                // Obtiene el identificador de cliente de la fila seleccionada
+                int identificacionCliente = Convert.ToInt32(DataViewClientes.SelectedRows[0].Cells["IdCliente"].Value);
+
+                // Prepara la consulta SQL para eliminar el cliente
+                string query = "DELETE FROM Clientes WHERE identificacionCliente = @identificacionCliente";
+                SqlCommand comando = new SqlCommand(query, conexion.ConectarBD);
+                comando.Parameters.AddWithValue("@identificacionCliente", identificacionCliente);
+
+                try
+                {
+                    conexion.abrir();
+                    int filasAfectadas = comando.ExecuteNonQuery();
+                    if (filasAfectadas > 0)
+                    {
+                        MessageBox.Show("El cliente ha sido eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Actualiza la vista del DataGridView después de eliminar el cliente
+                        actualizarDataView();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontró el cliente seleccionado en la base de datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar el cliente: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    conexion.cerrar();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila para eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
     }
 }
