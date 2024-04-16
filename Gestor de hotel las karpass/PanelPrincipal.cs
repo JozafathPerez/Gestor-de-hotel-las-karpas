3c6d5ea093cf6479c8dad5a0e55de488ca000219
KeingellMoodie <80055929+KeingellMoodie@users.noreply.github.com>
2024-04-15 22:49:24 -0600

    Documentacion interna PanelPrincipal.cs

--- Gestor de hotel las karpass/PanelPrincipal.cs
+++ Gestor de hotel las karpass/PanelPrincipal.cs
@@ -19,6 +19,12 @@ namespace Gestor_de_hotel_las_karpass
         private FuncionesAux funcionesAux;
         private int idEmpleado;
 
+        /*******************************************************
+        * Nombre: PanelInicioSesion
+        * Descripcion: almacena un objeto de tipo ConexionBD y inicializa la ventana.
+        * Entradas: 
+        * Salidad: 
+        * *******************************************************/
         public PanelPrincipal(int idEmpleado)
         {
             InitializeComponent();
@@ -31,7 +37,12 @@ namespace Gestor_de_hotel_las_karpass
             if (idRol != 1) { BtPersonal.Visible = false; }
         }
 
-        // Función para cambiar el color del botón y restaurar el color del último botón presionado
+        /*******************************************************
+        * Nombre: CambiarColorBoton
+        * Descripcion: Función para cambiar el color del botón y restaurar el color del último botón presionado.
+        * Entradas: objeto (Button) el cual es precionado.
+        * Salidad: 
+        * *******************************************************/
         private void CambiarColorBoton(Button botonPresionado)
         {
             if (ultimoBoton != null)
@@ -43,6 +54,13 @@ namespace Gestor_de_hotel_las_karpass
             ultimoBoton = botonPresionado; // Actualizar el último botón presionado
         }
 
+        /*******************************************************
+        * Nombre: BtCliente_Click
+        * Descripcion: Llama una funcion para cambiar el color de un boton.
+        * Entradas: objeto (object) en el cual se va realizar el evento 
+        * y el evento a realizar en el (EventArgs).
+        * Salidad: 
+        * *******************************************************/
         private void BtCliente_Click(object sender, EventArgs e)
         {
             CambiarColorBoton(BtCliente);
@@ -50,6 +68,13 @@ namespace Gestor_de_hotel_las_karpass
             cargarForm(new ClientesForm(idEmpleado));
         }
 
+        /*******************************************************
+        * Nombre: BtReservas_Click
+        * Descripcion: Llama una funcion para cambiar el color de un boton.
+        * Entradas: objeto (object) en el cual se va realizar el evento 
+        * y el evento a realizar en el (EventArgs).
+        * Salidad: 
+        * *******************************************************/
         private void BtReservas_Click(object sender, EventArgs e)
         {
             CambiarColorBoton(BtReservas);
@@ -57,6 +82,13 @@ namespace Gestor_de_hotel_las_karpass
             cargarForm(new ReservasForm(idEmpleado));
         }
 
+        /*******************************************************
+        * Nombre: BtReportería_Click
+        * Descripcion: Llama una funcion para cambiar el color de un boton.
+        * Entradas: objeto (object) en el cual se va realizar el evento 
+        * y el evento a realizar en el (EventArgs).
+        * Salidad: 
+        * *******************************************************/
         private void BtReportería_Click(object sender, EventArgs e)
         {
             CambiarColorBoton(BtReportería);
@@ -64,6 +96,13 @@ namespace Gestor_de_hotel_las_karpass
             cargarForm(new ReporteriaForm());
         }
 
+        /*******************************************************
+        * Nombre: BtPersonal_Click
+        * Descripcion: Llama una funcion para cambiar el color de un boton.
+        * Entradas: objeto (object) en el cual se va realizar el evento 
+        * y el evento a realizar en el (EventArgs).
+        * Salidad: 
+        * *******************************************************/
         private void BtPersonal_Click(object sender, EventArgs e)
         {
             CambiarColorBoton(BtPersonal);
@@ -71,8 +110,13 @@ namespace Gestor_de_hotel_las_karpass
             cargarForm(new PersonalForm());
         }
 
-        // Función para cargar los Forms (los sub-menus) y colocarlos en el PanelAreaDeTrabajo
-        // También remueve lo que estaba antes en el panel!!
+        /*******************************************************
+        * Nombre: BtPersonal_Click
+        * Descripcion: Función para cargar los Forms (los sub-menus) y colocarlos en el PanelAreaDeTrabajo
+        * También remueve lo que estaba antes en el panel.
+        * Entradas: objeto (object) Form a cargar.
+        * Salidad: 
+        * *******************************************************/
         public void cargarForm(object Form)
         {
             if (this.PanelAreaDeTrabajo.Controls.Count > 0)
